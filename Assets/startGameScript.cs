92766fa3e7cf5218cf2f5d6de25bddf5fcd92f38
BorisTheLizard <99308714+BorisTheLizard@users.noreply.github.com>
2024-06-02 16:13:40 -0700

    patch

--- Assets/startGameScript.cs
+++ Assets/startGameScript.cs
@@ -8,12 +8,14 @@ public class startGameScript : MonoBehaviour
 	[SerializeField] Animator anim;
 	[SerializeField] GameObject timeLine;
 	[SerializeField] GameObject fadeIn;
+	[SerializeField] GameObject book;
 
 	[SerializeField] sceneManagement _sm;
 	public void startGame()
 	{
 		anim.SetTrigger("off");
 		timeLine.SetActive(true);
+		book.SetActive(false);
 	}
 
 	public void launchLvl1()
